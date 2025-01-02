0d113f3bf9e5ec20de8c05408c23967f9626380c
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-01-02 23:59:38 +0530

    Time: 2 ms (77.34%), Space: 44.2 MB (72.66%) - LeetHub

---
+++ 0919-projection-area-of-3d-shapes/0919-projection-area-of-3d-shapes.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int projectionArea(int[][] grid) {
+        int[] front=new int[grid.length];
+        int base=0,back=0,total=0;
+        for(int i=0;i<grid.length;i++)
+        {
+            int temp=0;
+            for(int j=0;j<grid.length;j++)
+            {
+                if(grid[i][j]!=0)
+                {
+                    base+=1;
+                    if(grid[i][j]>temp)
+                    {
+                        temp=grid[i][j];
+                    }
+                    if(grid[i][j]>front[j])
+                    {
+                        front[j]=grid[i][j];
+                    }
+                }
+            }
+            total+=temp;
+        }
+        for(int i=0;i<front.length;i++)
+        {
+            back+=front[i];
+        }
+        return total+back+base;
+    }
+}
